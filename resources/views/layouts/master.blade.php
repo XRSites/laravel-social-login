e7cf4ab47c12f820c3b2d455ee56ecac22af44ec
Guilherme Luiz Argentino Silva <info@xrs.com.br>
2017-12-09 22:06:09 -0200

    Layout Adjustments. Simplifying and Centralizing.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,36 +11,7 @@
     </head>
     <body>
         <div id="app">
-            <nav class="navbar navbar-default navbar-static-top">
-                <div class="container">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-
-                        <!-- Branding Image -->
-                        <a class="navbar-brand" href="{{ url('/') }}">
-                            {{ config('app.name', 'Laravel') }}
-                        </a>
-                    </div>
-                    <div id="navbar" class="collapse navbar-collapse">
-                        <ul class="nav navbar-nav navbar-right">
-                            @if(auth()->check())
-                            @include('partials.user-menu')
-                            @else
-                            <li>
-                                <a href="{{ route('login') }}">
-                                    Sign In <i class="fa fa-sign-in"></i>
-                                </a>
-                            </li>
-                            @endif
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            @include('partials.top-navbar')
 
             <div class="container">
                 @yield('content')
--- resources/views/partials/user-menu.blade.php
+++ resources/views/partials/user-menu.blade.php
@@ -1,5 +1,8 @@
 <li class="dropdown">
-    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">logged as {{ auth()->user()->name }} <img alt="@rrantz" class="avatar float-left mr-1" src="{{ auth()->user()->avatar }}" height="20" width="20"> <span class="caret"></span></a>
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+        {{ auth()->user()->name }} <img alt="avatar" class="img-rounded" src="{{ auth()->user()->avatar }}" height="20" width="20"> 
+        <span class="caret"></span>
+    </a>
     <ul class="dropdown-menu" role="menu">
         <li><a href="{{ action('HomeController@showDashboard') }}">Dashboard</a></li>
         <li role="separator" class="divider"></li>
