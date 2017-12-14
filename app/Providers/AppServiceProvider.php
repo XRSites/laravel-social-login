aae1833c9f212dce8f1bb158cf1d561efa6a7237
Guilherme Luiz Argentino Silva <info@xrs.com.br>
2017-12-14 19:46:56 -0200

    Bugfixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes
+        Schema::defaultStringLength(191);
     }
 
     /**
